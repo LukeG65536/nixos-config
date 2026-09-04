59c757237878097c4e2b85bd1868090785a7ac95
= <=>
2026-09-04 11:06:45 -0600

    auto commit gen 92

--- home/cli.nix
+++ home/cli.nix
@@ -36,6 +36,12 @@
   ];
 
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/duffy/nixos-config";
+  };
 
   
   programs.helix = {
