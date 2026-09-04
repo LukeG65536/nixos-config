e12483210d77107447d89716e7dd72a5080c382c
= <=>
2026-09-04 12:01:28 -0600

    auto commit gen 95

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -105,7 +105,7 @@
   services.flatpak.enable = true;
 
   programs.fish.shellAliases = {
-    re = "sudo nixos-rebuild switch --flake ~/nixos-config/#laptop";
-    gre = "cd ~/nixos-config; git add -A; git commit -m \"auto commit gen \"(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -1 | awk '{print $1}'); git push; sudo nixos-rebuild switch --flake ~/nixos-config/#laptop"; 
+    re = "nh os switch -H laptop";
+    gre = "cd ~/nixos-config; git add -A; git commit -m \"auto commit gen \"(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | tail -1 | awk '{print $1}'); git push; nh os switch -H laptop"; 
   };
 }
